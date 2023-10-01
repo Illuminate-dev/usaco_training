09721dd0ba59a0359acf24ef13355f28ab807bc3
Henry Beveridge <henrydbeveridge@gmail.com>
2023-09-30 23:37:38 -0700

    add plan for castle

---
+++ Chatper2/Section1.1/castle.cpp
@@ -0,0 +1,21 @@
+/*
+ID: henrydb1
+PROG: castle
+LANG: C++
+*/
+
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  ifstream cin("castle.in");
+  ofstream cout("castle.out");
+
+  // read in castle input somehow
+  // use unionfind to create disjoint set
+  // this gets us # of rooms and size
+  // then for each pair of rooms, find wall that can be removed + size of new
+  //    room
+}
