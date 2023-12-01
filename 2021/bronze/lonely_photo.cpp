b6ab9f2ce651bdd82b059c6b549784df4d908a39
Henry Beveridge <henrydbeveridge@gmail.com>
2023-11-30 23:20:03 -0800

    solve lonely photo

--- 2021/bronze/lonely_photo.cpp
+++ 2021/bronze/lonely_photo.cpp
@@ -2,6 +2,8 @@
 
 using namespace std;
 
+#define ll long long
+
 int main() {
   int N;
   string cows;
@@ -17,9 +19,53 @@ int main() {
     }
   }
 
-  int ans = 0;
-  int nextHIndexIndex = 0;
-  int nextGIndexIndex = 0;
+  ll ans = 0;
+  int hidx = 0;
+  int gidx = 0;
   for (int i = 0; i < N - 2; i++) {
+    if (hidx < hIndex.size() && i > hIndex[hidx]) {
+      hidx++;
+    }
+    if (gidx < gIndex.size() && i > gIndex[gidx]) {
+      gidx++;
+    }
+
+    int curH, nextH;
+    if (hidx < hIndex.size()) {
+      curH = hIndex[hidx];
+    } else {
+      curH = N;
+    }
+    if (hidx + 1 < hIndex.size()) {
+      nextH = hIndex[hidx + 1];
+    } else {
+      nextH = N;
+    }
+
+    int curG, nextG;
+    if (gidx < gIndex.size()) {
+      curG = gIndex[gidx];
+    } else {
+      curG = N;
+    }
+    if (gidx + 1 < gIndex.size()) {
+      nextG = gIndex[gidx + 1];
+    } else {
+      nextG = N;
+    }
+
+    int subtractor = 0;
+    if (nextH < curG && curG - i + 1 > 3) {
+      int numHs = curG - i;
+      subtractor = numHs - 2;
+    } else if (nextG < curH && curH - i + 1 > 3) {
+      int numGs = curH - i;
+      subtractor = numGs - 2;
+    }
+    int len = max(nextH, nextG) - i;
+
+    ans += len - 2 - subtractor;
   }
+
+  cout << ans << endl;
 }
